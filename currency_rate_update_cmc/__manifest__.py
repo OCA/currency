f7646cab6485a8e8d1f70e10f5a288440c446bfd
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-03-20 13:55:25 +0100

    add currency_rate_update_cmc

---
+++ currency_rate_update_cmc/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2018 Fork Sand Inc.
+# Copyright 2018 Ross Golder
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Currency Rate Update Coin Market Cap',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'summary': 'Allows to download crypto currency exchange rates from '
+               'Coin Market Cap',
+    'author': 'Eficent,'
+              'Odoo Community Association (OCA),',
+    'website': 'https://github.com/OCA/currency',
+    'license': 'AGPL-3',
+    'depends': [
+        'currency_rate_update',
+    ],
+    "installable": True
+}
