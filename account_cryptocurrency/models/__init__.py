bad1bc92b5f25cb26ab858fb096256e90c0f7d3e
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-03-21 16:08:35 +0100

    add account_cryptocurrency

---
+++ account_cryptocurrency/models/__init__.py
@@ -0,0 +1,7 @@
+from . import res_currency
+from . import account_payment
+from . import res_currency_move
+from . import res_currency_move_line
+from . import account_move
+from . import account_account
+from . import account_bank_statement
