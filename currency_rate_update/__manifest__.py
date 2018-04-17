d40647e98bfc9f3c42f47f051b5b747aa9f6e0dc
Fekete Mihai <mihai.fekete@forbiom.eu>
2018-04-17 12:10:15 -0700

    Update view, odoo compatibility.
    Update version.
    
    Update view.

--- currency_rate_update/__manifest__.py
+++ currency_rate_update/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Currency Rate Update",
-    "version": "11.0.0.0.0",
+    "version": "11.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "website": "http://camptocamp.com",
     "license": "AGPL-3",
