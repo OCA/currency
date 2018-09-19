84c401da152fc8a53902d3da28c46a818150f752
angelmoya <amoyapardo@gmail.com>
2018-09-19 20:33:03 +0200

    ADD currency_rate_update_ch_admin

---
+++ currency_rate_update_ch_admin/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 PESOL
+# Copyright 2018 Angel Moya <angel.moya@pesol.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Currency Rate Update Admin.ch',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'summary': 'Allows to download crypto currency exchange rates from '
+               'Admin.ch',
+    'author': 'PESOL,'
+              'Odoo Community Association (OCA),',
+    'website': 'https://github.com/OCA/currency',
+    'license': 'AGPL-3',
+    'depends': [
+        'currency_rate_update',
+    ],
+    "installable": True
+}
---
+++ currency_rate_update_ch_admin/services/__init__.py
@@ -0,0 +1,1 @@
+from . import update_service_ch_admin
