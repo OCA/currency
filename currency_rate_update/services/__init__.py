0166ef4849952f39209c7bf673bd3c5ec836774e
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-02-13 11:32:04 +0200

    [MIG] currency_rate_update: Migration to 12.0

--- currency_rate_update/services/__init__.py
+++
@@ -1,3 +0,0 @@
-# Note, that CA_BOC getter seems not to be working
-# from . import update_service_CA_BOC
-from . import update_service_ECB
--- currency_rate_update/tests/test_currency_rate_update.py
+++ currency_rate_update/tests/test_currency_rate_update.py
@@ -1,96 +1,103 @@
-# © 2017 Comunitea
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2017 Comunitea
+# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from dateutil.relativedelta import relativedelta
 
 from odoo.tests import common
-from odoo import exceptions, fields
+from odoo import fields
 
 
 class TestCurrencyRateUpdate(common.SavepointCase):
 
     def setUp(self):
-        super(TestCurrencyRateUpdate, self).setUp()
-        self.company = self.env['res.company'].create({
+        super().setUp()
+
+        self.Company = self.env['res.company']
+        self.CurrencyRate = self.env['res.currency.rate']
+        self.CurrencyRateProvider = self.env['res.currency.rate.provider']
+
+        self.today = fields.Date.today()
+        self.eur_currency = self.env.ref('base.EUR')
+        self.usd_currency = self.env.ref('base.USD')
+        self.company = self.Company.create({
             'name': 'Test company',
-            'currency_id': self.env.ref('base.EUR').id,
+            'currency_id': self.eur_currency.id,
         })
         self.env.user.company_ids += self.company
         self.env.user.company_id = self.company
-        self.currency = self.env.ref('base.USD')
-        self.update_service = self.env['currency.rate.update.service'].create({
+        self.ecb_provider = self.CurrencyRateProvider.create({
             'service': 'ECB',
-            'currency_to_update':
-                [(4, self.currency.id),
-                 (4, self.env.user.company_id.currency_id.id)]
+            'currency_ids': [
+                (4, self.usd_currency.id),
+                (4, self.env.user.company_id.currency_id.id),
+            ],
         })
-        currency_rates = self.env['res.currency.rate'].search(
-            [('currency_id', '=', self.currency.id)])
-        currency_rates.unlink()
+        self.CurrencyRate.search([]).unlink()
 
-    def test_currency_rate_update_ECB(self):
-        self.update_service.refresh_currency()
-        currency_rates = self.env['res.currency.rate'].search(
-            [('currency_id', '=', self.currency.id)])
-        self.assertTrue(currency_rates)
+    def test_update_ECB_today(self):
+        """No checks are made since today may not be a banking day"""
+        self.ecb_provider._update(self.today, self.today)
+        self.CurrencyRate.search([
+            ('currency_id', '=', self.usd_currency.id),
+        ]).unlink()
 
-    def test_constrains_max_delta_days(self):
-        with self.assertRaises(exceptions.ValidationError):
-            self.update_service.write({'max_delta_days': -5})
+    def test_update_ECB_month(self):
+        self.ecb_provider._update(
+            self.today - relativedelta(months=1),
+            self.today
+        )
 
-    def test_constrains_interval_number(self):
-        with self.assertRaises(exceptions.ValidationError):
-            self.update_service.write({'interval_number': -5})
+        rates = self.CurrencyRate.search([
+            ('currency_id', '=', self.usd_currency.id),
+        ], limit=1)
+        self.assertTrue(rates)
 
-    def test_onchange_service(self):
-        new_service = self.env['currency.rate.update.service'].new(
-            {'service': 'ECB'})
-        new_service._onchange_service()
-        self.assertEqual(len(new_service.currency_list), 2)
+        self.CurrencyRate.search([
+            ('currency_id', '=', self.usd_currency.id),
+        ]).unlink()
 
-    def test_deactivate_update(self):
-        before_message_count = len(self.update_service.message_ids)
-        self.update_service.write({'interval_number': 0})
-        self.assertEqual(before_message_count + 1,
-                         len(self.update_service.message_ids))
+    def test_update_ECB_year(self):
+        self.ecb_provider._update(
+            self.today - relativedelta(years=1),
+            self.today
+        )
 
-    def test_currency_rate_update_cron(self):
-        self.update_service.next_run = fields.Date.today()
-        self.env['currency.rate.update.service']._run_currency_update()
-        self.update_service.refresh_currency()
-        currency_rates = self.env['res.currency.rate'].search(
-            [('currency_id', '=', self.currency.id)])
-        self.assertTrue(currency_rates)
+        rates = self.CurrencyRate.search([
+            ('currency_id', '=', self.usd_currency.id),
+        ], limit=1)
+        self.assertTrue(rates)
 
-    def test_currency_rate_update_USD_EUR(self):
-        self.env.user.company_id.currency_id = self.env.ref('base.USD')
-        self.euro = self.env.ref('base.EUR')
-        self.update_service.write({
-            'currency_to_update': [(4, self.euro.id)]
-        })
-        currency_rates = self.env['res.currency.rate'].search(
-            [('currency_id', '=', self.euro.id)])
-        currency_rates.unlink()
-        main_currency_rates = self.env['res.currency.rate'].search(
-            [('currency_id', '=', self.env.user.company_id.currency_id.id)])
-        main_currency_rates.unlink()
-        self.update_service.refresh_currency()
-        currency_rates = self.env['res.currency.rate'].search(
-            [('currency_id', '=', self.euro.id)])
-        self.assertTrue(currency_rates)
+        self.CurrencyRate.search([
+            ('currency_id', '=', self.usd_currency.id),
+        ]).unlink()
 
-    def test_currency_rate_update_USD_CHF(self):
-        self.env.user.company_id.currency_id = self.env.ref('base.USD')
-        self.chf = self.env.ref('base.CHF')
-        self.env.ref('base.CHF').active = True
-        self.update_service.write({
-            'currency_to_update': [(4, self.chf.id)]
-        })
-        currency_rates = self.env['res.currency.rate'].search(
-            [('currency_id', '=', self.chf.id)])
-        currency_rates.unlink()
-        main_currency_rates = self.env['res.currency.rate'].search(
-            [('currency_id', '=', self.env.user.company_id.currency_id.id)])
-        main_currency_rates.unlink()
-        self.update_service.refresh_currency()
-        currency_rates = self.env['res.currency.rate'].search(
-            [('currency_id', '=', self.chf.id)])
-        self.assertTrue(currency_rates)
+    def test_update_ECB_scheduled(self):
+        self.ecb_provider.next_run = fields.Date.today()
+        self.ecb_provider._scheduled_update()
+
+        rates = self.CurrencyRate.search([
+            ('currency_id', '=', self.usd_currency.id),
+        ])
+        self.assertEquals(len(rates), 1)
+
+        self.CurrencyRate.search([
+            ('currency_id', '=', self.usd_currency.id),
+        ]).unlink()
+
+    def test_update_ECB_no_base_update(self):
+        self.ecb_provider.next_run = fields.Date.today()
+        self.ecb_provider._scheduled_update()
+
+        rates = self.CurrencyRate.search([
+            ('company_id', '=', self.company.id),
+            ('currency_id', 'in', [
+                self.usd_currency.id,
+                self.eur_currency.id,
+            ]),
+        ])
+        self.assertEquals(len(rates), 1)
+
+        self.CurrencyRate.search([
+            ('company_id', '=', self.company.id),
+        ]).unlink()
