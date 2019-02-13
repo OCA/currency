f7433f1dbb7e843e6a767665ba0b91a54b4394b8
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-02-13 08:43:17 +0200

    [MIG] currency_rate_inverted: Migration to 12.0

--- currency_rate_inverted/models/res_currency.py
+++ currency_rate_inverted/models/res_currency.py
@@ -2,6 +2,7 @@
 # Copyright 2015 Techrifiv Solutions Pte Ltd
 # Copyright 2015 Statecraft Systems Pte Ltd
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from odoo import api, fields, models
 
 
@@ -9,18 +10,30 @@ class ResCurrency(models.Model):
     _inherit = 'res.currency'
 
     rate_inverted = fields.Boolean(
-        'Inverted exchange rate',
-        company_dependent=True)
+        string='Inverted exchange rate',
+        company_dependent=True,
+    )
 
     @api.model
-    def _get_conversion_rate(self, from_currency, to_currency):
-        res = super(ResCurrency, self)._get_conversion_rate(from_currency,
-                                                            to_currency)
+    def _get_conversion_rate(self, from_currency, to_currency, company, date):
+        rate = super()._get_conversion_rate(
+            from_currency,
+            to_currency,
+            company,
+            date
+        )
 
-        if (
-            from_currency.rate_inverted and to_currency.rate_inverted or
-                not from_currency.rate_inverted and
-                not to_currency.rate_inverted):
-            return res
-        else:
-            return 1/res
+        if not from_currency.rate_inverted and not to_currency.rate_inverted:
+            return rate
+        elif from_currency.rate_inverted and to_currency.rate_inverted:
+            return 1 / rate
+
+        currency_rates = (
+            from_currency + to_currency
+        )._get_rates(company, date)
+        l_rate = currency_rates.get(to_currency.id)
+        r_rate = currency_rates.get(from_currency.id)
+        if not from_currency.rate_inverted and to_currency.rate_inverted:
+            return 1 / (l_rate * r_rate)
+        elif from_currency.rate_inverted and not to_currency.rate_inverted:
+            return l_rate * r_rate
