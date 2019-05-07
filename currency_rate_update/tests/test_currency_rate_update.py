d5c0b59e967cb0efa5d7e2ed95637e5d339a0c3a
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-05-07 22:02:28 +0300

    [IMP] currency_rate_update: use last_successful_run

--- currency_rate_update/tests/test_currency_rate_update.py
+++ currency_rate_update/tests/test_currency_rate_update.py
@@ -2,6 +2,7 @@
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+from datetime import date
 from dateutil.relativedelta import relativedelta
 from unittest import mock
 
@@ -94,8 +95,10 @@ class TestCurrencyRateUpdate(common.SavepointCase):
         ]).unlink()
 
     def test_update_ECB_scheduled(self):
+        self.ecb_provider.interval_type = 'days'
+        self.ecb_provider.interval_number = 14
         self.ecb_provider.next_run = (
-            fields.Date.today() - relativedelta(days=15)
+            self.today - relativedelta(days=1)
         )
         self.ecb_provider._scheduled_update()
 
@@ -109,8 +112,10 @@ class TestCurrencyRateUpdate(common.SavepointCase):
         ]).unlink()
 
     def test_update_ECB_no_base_update(self):
+        self.ecb_provider.interval_type = 'days'
+        self.ecb_provider.interval_number = 14
         self.ecb_provider.next_run = (
-            fields.Date.today() - relativedelta(days=15)
+            self.today - relativedelta(days=1)
         )
         self.ecb_provider._scheduled_update()
 
@@ -122,6 +127,46 @@ class TestCurrencyRateUpdate(common.SavepointCase):
             ]),
         ], limit=1)
         self.assertTrue(rates)
+
+        self.CurrencyRate.search([
+            ('company_id', '=', self.company.id),
+        ]).unlink()
+
+    def test_update_ECB_sequence(self):
+        self.ecb_provider.interval_type = 'days'
+        self.ecb_provider.interval_number = 1
+        self.ecb_provider.last_successful_run = None
+        self.ecb_provider.next_run = date(2019, 4, 1)
+
+        self.ecb_provider._scheduled_update()
+        self.assertEqual(
+            self.ecb_provider.last_successful_run,
+            date(2019, 4, 1)
+        )
+        self.assertEqual(
+            self.ecb_provider.next_run,
+            date(2019, 4, 2)
+        )
+        rates = self.CurrencyRate.search([
+            ('company_id', '=', self.company.id),
+            ('currency_id', '=', self.usd_currency.id),
+        ])
+        self.assertEqual(len(rates), 1)
+
+        self.ecb_provider._scheduled_update()
+        self.assertEqual(
+            self.ecb_provider.last_successful_run,
+            date(2019, 4, 2)
+        )
+        self.assertEqual(
+            self.ecb_provider.next_run,
+            date(2019, 4, 3)
+        )
+        rates = self.CurrencyRate.search([
+            ('company_id', '=', self.company.id),
+            ('currency_id', '=', self.usd_currency.id),
+        ])
+        self.assertEqual(len(rates), 2)
 
         self.CurrencyRate.search([
             ('company_id', '=', self.company.id),
