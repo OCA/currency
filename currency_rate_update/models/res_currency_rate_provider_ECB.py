27643616e60ca04cc8154dfec1ecb41ef658bc36
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-03-07 15:48:18 +0200

    [IMP] currency_rate_update: black, isort

--- currency_rate_update/models/res_currency_rate_provider_ECB.py
+++ currency_rate_update/models/res_currency_rate_provider_ECB.py
@@ -3,59 +3,93 @@
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+import xml.sax
 from collections import defaultdict
 from datetime import date, timedelta
 from urllib.request import urlopen
-import xml.sax
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class ResCurrencyRateProviderECB(models.Model):
-    _inherit = 'res.currency.rate.provider'
+    _inherit = "res.currency.rate.provider"
 
-    service = fields.Selection(
-        selection_add=[('ECB', 'European Central Bank')],
-    )
+    service = fields.Selection(selection_add=[("ECB", "European Central Bank")])
 
     @api.multi
     def _get_supported_currencies(self):
         self.ensure_one()
-        if self.service != 'ECB':
+        if self.service != "ECB":
             return super()._get_supported_currencies()  # pragma: no cover
 
         # List of currencies obrained from:
         # https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip
-        return \
-            [
-                'USD', 'JPY', 'BGN', 'CYP', 'CZK', 'DKK', 'EEK', 'GBP',
-                'HUF', 'LTL', 'LVL', 'MTL', 'PLN', 'ROL', 'RON', 'SEK',
-                'SIT', 'SKK', 'CHF', 'ISK', 'NOK', 'HRK', 'RUB', 'TRL',
-                'TRY', 'AUD', 'BRL', 'CAD', 'CNY', 'HKD', 'IDR', 'ILS',
-                'INR', 'KRW', 'MXN', 'MYR', 'NZD', 'PHP', 'SGD', 'THB',
-                'ZAR', 'EUR'
-            ]
+        return [
+            "USD",
+            "JPY",
+            "BGN",
+            "CYP",
+            "CZK",
+            "DKK",
+            "EEK",
+            "GBP",
+            "HUF",
+            "LTL",
+            "LVL",
+            "MTL",
+            "PLN",
+            "ROL",
+            "RON",
+            "SEK",
+            "SIT",
+            "SKK",
+            "CHF",
+            "ISK",
+            "NOK",
+            "HRK",
+            "RUB",
+            "TRL",
+            "TRY",
+            "AUD",
+            "BRL",
+            "CAD",
+            "CNY",
+            "HKD",
+            "IDR",
+            "ILS",
+            "INR",
+            "KRW",
+            "MXN",
+            "MYR",
+            "NZD",
+            "PHP",
+            "SGD",
+            "THB",
+            "ZAR",
+            "EUR",
+        ]
 
     @api.multi
     def _obtain_rates(self, base_currency, currencies, date_from, date_to):
         self.ensure_one()
-        if self.service != 'ECB':
-            return super()._obtain_rates(base_currency, currencies, date_from,
-                                         date_to)  # pragma: no cover
+        if self.service != "ECB":
+            return super()._obtain_rates(
+                base_currency, currencies, date_from, date_to
+            )  # pragma: no cover
         invert_calculation = False
-        if base_currency != 'EUR':
+        if base_currency != "EUR":
             invert_calculation = True
             if base_currency not in currencies:
                 currencies.append(base_currency)
 
         # Depending on the date range, different URLs are used
-        url = 'https://www.ecb.europa.eu/stats/eurofxref'
+        url = "https://www.ecb.europa.eu/stats/eurofxref"
         if date_from == date_to and date_from == date.today():
-            url = url + '/eurofxref-daily.xml'
+            url = url + "/eurofxref-daily.xml"
         elif (date.today() - date_from) / timedelta(days=90) < 1.0:
-            url = url + '/eurofxref-hist-90d.xml'
+            url = url + "/eurofxref-hist-90d.xml"
         else:
-            url = url + '/eurofxref-hist.xml'
+            url = url + "/eurofxref-hist.xml"
 
         handler = EcbRatesHandler(currencies, date_from, date_to)
         with urlopen(url) as response:
@@ -65,8 +99,8 @@ class ResCurrencyRateProviderECB(models.Model):
             for k in content.keys():
                 base_rate = float(content[k][base_currency])
                 for rate in content[k].keys():
-                    content[k][rate] = str(float(content[k][rate])/base_rate)
-                content[k]['EUR'] = str(1.0/base_rate)
+                    content[k][rate] = str(float(content[k][rate]) / base_rate)
+                content[k]["EUR"] = str(1.0 / base_rate)
         return content
 
 
@@ -79,13 +113,14 @@ class EcbRatesHandler(xml.sax.ContentHandler):
         self.content = defaultdict(dict)
 
     def startElement(self, name, attrs):
-        if name == 'Cube' and 'time' in attrs:
-            self.date = fields.Date.from_string(attrs['time'])
-        elif name == 'Cube' and \
-                all([x in attrs for x in ['currency', 'rate']]):
-            currency = attrs['currency']
-            rate = attrs['rate']
-            if (self.date_from is None or self.date >= self.date_from) and \
-                    (self.date_to is None or self.date <= self.date_to) and \
-                    currency in self.currencies:
+        if name == "Cube" and "time" in attrs:
+            self.date = fields.Date.from_string(attrs["time"])
+        elif name == "Cube" and all([x in attrs for x in ["currency", "rate"]]):
+            currency = attrs["currency"]
+            rate = attrs["rate"]
+            if (
+                (self.date_from is None or self.date >= self.date_from)
+                and (self.date_to is None or self.date <= self.date_to)
+                and currency in self.currencies
+            ):
                 self.content[self.date.isoformat()][currency] = rate
