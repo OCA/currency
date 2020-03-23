6f7ee77eb7725871a88aeb7ef20d616aedecc89c
Alexey Pelykh <alexey.pelykh@gmail.com>
2020-03-23 08:23:36 +0100

    [FIX] currency_rate_update: catch any exception

--- currency_rate_update/models/res_currency_rate_provider.py
+++ currency_rate_update/models/res_currency_rate_provider.py
@@ -1,6 +1,6 @@
 # Copyright 2009-2016 Camptocamp
 # Copyright 2010 Akretion
-# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2019-2020 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import logging
@@ -122,10 +122,19 @@ class ResCurrencyRateProvider(models.Model):
                     date_from,
                     date_to,
                 ).items()
-            except Exception as e:
-                _logger.warning("Currency Rate Provider Failure: %s" % e)
+            except BaseException as e:
+                _logger.warning(
+                    'Currency Rate Provider "%s" failed to obtain data since'
+                    " %s until %s" % (provider.name, date_from, date_to,),
+                    exc_info=True,
+                )
                 provider.message_post(
-                    body=str(e), subject=_("Currency Rate Provider Failure")
+                    subject=_("Currency Rate Provider Failure"),
+                    body=_(
+                        'Currency Rate Provider "%s" failed to obtain data'
+                        " since %s until %s:\n%s"
+                    )
+                    % (provider.name, date_from, date_to, str(e) if e else _("N/A"),),
                 )
                 continue
 
