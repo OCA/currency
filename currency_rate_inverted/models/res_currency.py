4d457d187d9758f40715e4122adbd8915372323a
ps-tubtim <pimolnats@ecosoft.co.th>
2020-07-20 13:56:28 +0700

    [IMP] currency_rate_inverted: black, isort, prettier

--- currency_rate_inverted/models/res_currency.py
+++ currency_rate_inverted/models/res_currency.py
@@ -7,30 +7,22 @@ from odoo import api, fields, models
 
 
 class ResCurrency(models.Model):
-    _inherit = 'res.currency'
+    _inherit = "res.currency"
 
     rate_inverted = fields.Boolean(
-        string='Inverted exchange rate',
-        company_dependent=True,
+        string="Inverted exchange rate", company_dependent=True,
     )
 
     @api.model
     def _get_conversion_rate(self, from_currency, to_currency, company, date):
-        rate = super()._get_conversion_rate(
-            from_currency,
-            to_currency,
-            company,
-            date
-        )
+        rate = super()._get_conversion_rate(from_currency, to_currency, company, date)
 
         if not from_currency.rate_inverted and not to_currency.rate_inverted:
             return rate
         elif from_currency.rate_inverted and to_currency.rate_inverted:
             return 1 / rate
 
-        currency_rates = (
-            from_currency + to_currency
-        )._get_rates(company, date)
+        currency_rates = (from_currency + to_currency)._get_rates(company, date)
         l_rate = currency_rates.get(to_currency.id)
         r_rate = currency_rates.get(from_currency.id)
         if not from_currency.rate_inverted and to_currency.rate_inverted:
--- currency_rate_inverted/tests/test_currency_rate_inverted.py
+++ currency_rate_inverted/tests/test_currency_rate_inverted.py
@@ -8,108 +8,100 @@ from odoo.tests import common
 
 
 class TestCurrencyRateInverted(common.SavepointCase):
-
     def setUp(self):
         super().setUp()
 
         self.today = fields.Date.today()
-        self.Currency = self.env['res.currency']
+        self.Currency = self.env["res.currency"]
         self.SudoCurrency = self.Currency.sudo()
-        self.eur = self.SudoCurrency.search([('name', '=', 'EUR')], limit=1)
-        self.usd = self.SudoCurrency.search([('name', '=', 'USD')], limit=1)
+        self.eur = self.SudoCurrency.search([("name", "=", "EUR")], limit=1)
+        self.usd = self.SudoCurrency.search([("name", "=", "USD")], limit=1)
         self.gbp = self.SudoCurrency.search(
-            [('name', '=', 'GBP'), ('active', '=', False)],
-            limit=1
+            [("name", "=", "GBP"), ("active", "=", False)], limit=1
         )
         self.gbp.active = True
 
     def test_1(self):
-        self.eur.write({
-            'rate_ids': [
-                (0, False, {
-                    'name': self.today + timedelta(days=10),
-                }),
-            ],
-        })
-        self.usd.write({
-            'rate_ids': [
-                (0, False, {
-                    'name': self.today + timedelta(days=10),
-                    'rate': 0.8,
-                }),
-            ],
-        })
-        self.gbp.write({
-            'rate_ids': [
-                (0, False, {
-                    'name': self.today + timedelta(days=10),
-                    'rate': 1.1,
-                }),
-            ],
-        })
+        self.eur.write(
+            {"rate_ids": [(0, False, {"name": self.today + timedelta(days=10)})]}
+        )
+        self.usd.write(
+            {
+                "rate_ids": [
+                    (0, False, {"name": self.today + timedelta(days=10), "rate": 0.8})
+                ],
+            }
+        )
+        self.gbp.write(
+            {
+                "rate_ids": [
+                    (0, False, {"name": self.today + timedelta(days=10), "rate": 1.1})
+                ],
+            }
+        )
 
         self.usd.rate_inverted = True
         self.assertAlmostEqual(
-            self.eur.with_context({
-                'date': self.today + timedelta(days=10),
-            }).compute(1.0, self.usd),
+            self.eur.with_context({"date": self.today + timedelta(days=10)}).compute(
+                1.0, self.usd
+            ),
             1.25,
-            2
+            2,
         )
         self.assertAlmostEqual(
-            self.usd.with_context({
-                'date': self.today + timedelta(days=10),
-            }).compute(1.0, self.eur),
+            self.usd.with_context({"date": self.today + timedelta(days=10)}).compute(
+                1.0, self.eur
+            ),
             0.8,
-            2
+            2,
         )
 
         self.gbp.rate_inverted = True
         self.assertAlmostEqual(
-            self.gbp.with_context({
-                'date': self.today + timedelta(days=10),
-            }).compute(1.0, self.usd),
+            self.gbp.with_context({"date": self.today + timedelta(days=10)}).compute(
+                1.0, self.usd
+            ),
             1.38,
-            2
+            2,
         )
         self.gbp.rate_inverted = False
         self.assertAlmostEqual(
-            self.gbp.with_context({
-                'date': self.today + timedelta(days=10),
-            }).compute(1.0, self.usd),
+            self.gbp.with_context({"date": self.today + timedelta(days=10)}).compute(
+                1.0, self.usd
+            ),
             1.14,
-            2
+            2,
         )
 
         self.usd.rate_inverted = False
         self.assertAlmostEqual(
-            self.eur.with_context({
-                'date': self.today + timedelta(days=10),
-            }).compute(1.0, self.usd),
+            self.eur.with_context({"date": self.today + timedelta(days=10)}).compute(
+                1.0, self.usd
+            ),
             0.8,
-            2
+            2,
         )
         self.assertAlmostEqual(
-            self.usd.with_context({
-                'date': self.today + timedelta(days=10),
-            }).compute(1.0, self.eur),
+            self.usd.with_context({"date": self.today + timedelta(days=10)}).compute(
+                1.0, self.eur
+            ),
             1.25,
-            2
+            2,
         )
 
         self.gbp.rate_inverted = True
         self.assertAlmostEqual(
-            self.gbp.with_context({
-                'date': self.today + timedelta(days=10),
-            }).compute(1.0, self.usd),
+            self.gbp.with_context({"date": self.today + timedelta(days=10)}).compute(
+                1.0, self.usd
+            ),
             0.88,
-            2
+            2,
         )
         self.gbp.rate_inverted = False
         self.assertAlmostEqual(
-            self.gbp.with_context({
-                'date': self.today + timedelta(days=10),
-            }).compute(1.0, self.usd),
+            self.gbp.with_context({"date": self.today + timedelta(days=10)}).compute(
+                1.0, self.usd
+            ),
             0.73,
-            2
+            2,
         )
