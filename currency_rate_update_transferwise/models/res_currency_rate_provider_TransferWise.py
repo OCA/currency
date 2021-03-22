20cc98cf9132156b8e02275f502b9646ca806f6f
EntrepreneurAJ <aj@entrepreneuraj.com>
2021-03-22 19:17:46 +0000

    [IMP] currency_rate_update_transferwise: black, isort, prettier

--- currency_rate_update_transferwise/models/res_currency_rate_provider_TransferWise.py
+++ currency_rate_update_transferwise/models/res_currency_rate_provider_TransferWise.py
@@ -1,51 +1,54 @@
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from collections import defaultdict
-from datetime import timedelta
-import dateutil.parser
 import itertools
 import json
 import urllib.parse
 import urllib.request
+from collections import defaultdict
+from datetime import timedelta
 
-from odoo import models, fields, api, _
+import dateutil.parser
+
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
 class ResCurrencyRateProviderTransferWise(models.Model):
-    _inherit = 'res.currency.rate.provider'
+    _inherit = "res.currency.rate.provider"
 
-    service = fields.Selection(
-        selection_add=[('TransferWise', 'TransferWise.com')],
-    )
+    service = fields.Selection(selection_add=[("TransferWise", "TransferWise.com")],)
 
     @api.multi
     def _get_supported_currencies(self):
         self.ensure_one()
-        if self.service != 'TransferWise':
+        if self.service != "TransferWise":
             return super()._get_supported_currencies()  # pragma: no cover
 
-        url = 'https://api.transferwise.com/v1/rates'
+        url = "https://api.transferwise.com/v1/rates"
         data = json.loads(self._transferwise_provider_retrieve(url))
-        if 'error' in data and data['error']:
+        if "error" in data and data["error"]:
             raise UserError(
-                data['error_description']
-                if 'error_description' in data
-                else 'Unknown error'
+                data["error_description"]
+                if "error_description" in data
+                else "Unknown error"
             )
 
-        return list(set(itertools.chain.from_iterable(map(
-            lambda entry: [entry['source'], entry['target']],
-            data
-        ))))
+        return list(
+            set(
+                itertools.chain.from_iterable(
+                    map(lambda entry: [entry["source"], entry["target"]], data)
+                )
+            )
+        )
 
     @api.multi
     def _obtain_rates(self, base_currency, currencies, date_from, date_to):
         self.ensure_one()
-        if self.service != 'TransferWise':
-            return super()._obtain_rates(base_currency, currencies, date_from,
-                                         date_to)  # pragma: no cover
+        if self.service != "TransferWise":
+            return super()._obtain_rates(
+                base_currency, currencies, date_from, date_to
+            )  # pragma: no cover
 
         content = defaultdict(dict)
 
@@ -56,29 +59,29 @@ class ResCurrencyRateProviderTransferWise(models.Model):
             until = since + step
             while since <= date_to:
                 url = (
-                    'https://api.transferwise.com/v1/rates' +
-                    '?from=%(from)s' +
-                    '&to=%(to)s' +
-                    '&source=%(source)s' +
-                    '&target=%(target)s' +
-                    '&group=day'
+                    "https://api.transferwise.com/v1/rates"
+                    + "?from=%(from)s"
+                    + "&to=%(to)s"
+                    + "&source=%(source)s"
+                    + "&target=%(target)s"
+                    + "&group=day"
                 ) % {
-                    'source': base_currency,
-                    'target': currency,
-                    'from': str(since),
-                    'to': str(min(until, date_to)),
+                    "source": base_currency,
+                    "target": currency,
+                    "from": str(since),
+                    "to": str(min(until, date_to)),
                 }
                 data = json.loads(self._transferwise_provider_retrieve(url))
-                if 'error' in data and data['error']:
+                if "error" in data and data["error"]:
                     raise UserError(
-                        data['error_description']
-                        if 'error_description' in data
-                        else 'Unknown error'
+                        data["error_description"]
+                        if "error_description" in data
+                        else "Unknown error"
                     )
                 for entry in data:
-                    date = dateutil.parser.parse(entry['time']).date()
+                    date = dateutil.parser.parse(entry["time"]).date()
                     date_content = content[date.isoformat()]
-                    date_content[currency] = entry['rate']
+                    date_content[currency] = entry["rate"]
 
                 since += step
                 until += step
@@ -89,9 +92,7 @@ class ResCurrencyRateProviderTransferWise(models.Model):
     def _transferwise_provider_retrieve(self, url):
         self.ensure_one()
         with self._transferwise_provider_urlopen(url) as response:
-            content = response.read().decode(
-                response.headers.get_content_charset()
-            )
+            content = response.read().decode(response.headers.get_content_charset())
         return content
 
     @api.multi
@@ -99,13 +100,10 @@ class ResCurrencyRateProviderTransferWise(models.Model):
         self.ensure_one()
 
         if not self.company_id.transferwise_api_key:
-            raise UserError(_(
-                'No TransferWise.com credentials specified!'
-            ))
+            raise UserError(_("No TransferWise.com credentials specified!"))
 
         request = urllib.request.Request(url)
         request.add_header(
-            'Authorization',
-            'Bearer %s' % self.company_id.transferwise_api_key
+            "Authorization", "Bearer %s" % self.company_id.transferwise_api_key
         )
         return urllib.request.urlopen(request)
--- currency_rate_update_transferwise/tests/test_currency_rate_update_transferwise.py
+++ currency_rate_update_transferwise/tests/test_currency_rate_update_transferwise.py
@@ -1,54 +1,50 @@
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from dateutil.relativedelta import relativedelta
 from unittest import mock
 from urllib.error import HTTPError
+
+from dateutil.relativedelta import relativedelta
 
 from odoo import fields
 from odoo.exceptions import UserError
 from odoo.tests import common
 
-_module_ns = 'odoo.addons.currency_rate_update_transferwise'
+_module_ns = "odoo.addons.currency_rate_update_transferwise"
 _provider_class = (
     _module_ns
-    + '.models.res_currency_rate_provider_TransferWise'
-    + '.ResCurrencyRateProviderTransferWise'
+    + ".models.res_currency_rate_provider_TransferWise"
+    + ".ResCurrencyRateProviderTransferWise"
 )
 
 
 class TestResCurrencyRateProviderTransferWise(common.TransactionCase):
-
     def setUp(self):
         super().setUp()
 
-        self.Company = self.env['res.company']
-        self.CurrencyRate = self.env['res.currency.rate']
-        self.CurrencyRateProvider = self.env['res.currency.rate.provider']
+        self.Company = self.env["res.company"]
+        self.CurrencyRate = self.env["res.currency.rate"]
+        self.CurrencyRateProvider = self.env["res.currency.rate.provider"]
 
         self.today = fields.Date.today()
-        self.eur_currency = self.env.ref('base.EUR')
-        self.transferwise_provider = self.CurrencyRateProvider.create({
-            'service': 'TransferWise',
-            'currency_ids': [
-                (4, self.eur_currency.id),
-            ],
-        })
+        self.eur_currency = self.env.ref("base.EUR")
+        self.transferwise_provider = self.CurrencyRateProvider.create(
+            {"service": "TransferWise", "currency_ids": [(4, self.eur_currency.id)]}
+        )
         self.CurrencyRate.search([]).unlink()
 
     def test_supported_currencies(self):
-        mocked_response = (
-            """[
+        mocked_response = """[
     {
         "rate": 1.0,
         "source": "EUR",
         "target": "EUR",
         "time": "2019-01-01T00:00:00+0000"
     }
-]""")
+]"""
         supported_currencies = []
         with mock.patch(
-            _provider_class + '._transferwise_provider_retrieve',
+            _provider_class + "._transferwise_provider_retrieve",
             return_value=mocked_response,
         ):
             supported_currencies = (
@@ -58,31 +54,28 @@ class TestResCurrencyRateProviderTransferWise(common.TransactionCase):
 
     def test_update(self):
         date = self.today - relativedelta(days=1)
-        mocked_response = (
-            """[
-    {
+        mocked_response = """[
+    {{
         "rate": 0.86995,
         "source": "USD",
         "target": "EUR",
-        "time": "%(date)sT00:00:00+0000"
-    }
-]""" % {
-                'date': str(date),
-            })
+        "time": "{date}T00:00:00+0000"
+    }}
+]""".format(
+            date=str(date),
+        )
         with mock.patch(
-            _provider_class + '._transferwise_provider_retrieve',
+            _provider_class + "._transferwise_provider_retrieve",
             return_value=mocked_response,
         ):
             self.transferwise_provider._update(date, date)
 
-        rates = self.CurrencyRate.search([
-            ('currency_id', '=', self.eur_currency.id),
-        ], limit=1)
+        rates = self.CurrencyRate.search(
+            [("currency_id", "=", self.eur_currency.id)], limit=1
+        )
         self.assertTrue(rates)
 
-        self.CurrencyRate.search([
-            ('currency_id', '=', self.eur_currency.id),
-        ]).unlink()
+        self.CurrencyRate.search([("currency_id", "=", self.eur_currency.id)]).unlink()
 
     def test_no_credentials(self):
         self.env.user.company_id.transferwise_api_key = None
@@ -90,11 +83,8 @@ class TestResCurrencyRateProviderTransferWise(common.TransactionCase):
             self.transferwise_provider._get_supported_currencies()
 
     def test_bad_credentials(self):
-        self.env.user.company_id.transferwise_api_key = 'bad'
+        self.env.user.company_id.transferwise_api_key = "bad"
         with self.assertRaises(HTTPError):
             self.transferwise_provider._obtain_rates(
-                'USD',
-                ['EUR'],
-                self.today,
-                self.today,
+                "USD", ["EUR"], self.today, self.today,
             )
