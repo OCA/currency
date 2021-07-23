e3d566bc34c778034a30eed509941c9b2299db60
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2021-07-23 17:07:42 +0200

    [IMP] currency_rate_update_cmc: black, isort, prettier

--- currency_rate_update_cmc/__manifest__.py
+++ currency_rate_update_cmc/__manifest__.py
@@ -4,20 +4,19 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Currency Rate Update Coin Market Cap',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting & Finance',
-    'summary': 'Allows to download crypto currency exchange rates from '
-               'Coin Market Cap',
-    'author': 'ForgeFlow S.L.,'
-              'Odoo Community Association (OCA),',
-    'website': 'https://github.com/OCA/currency',
-    'license': 'AGPL-3',
-    'depends': [
-        'currency_rate_update',
+    "name": "Currency Rate Update Coin Market Cap",
+    "version": "12.0.1.0.0",
+    "category": "Accounting & Finance",
+    "summary": "Allows to download crypto currency exchange rates from "
+    "Coin Market Cap",
+    "author": "ForgeFlow S.L.," "Odoo Community Association (OCA),",
+    "website": "https://github.com/OCA/currency",
+    "license": "AGPL-3",
+    "depends": [
+        "currency_rate_update",
     ],
-    'data': [
-        'wizards/res_currency_rate_update_wizard.xml',
+    "data": [
+        "wizards/res_currency_rate_update_wizard.xml",
     ],
-    "installable": True
+    "installable": True,
 }
