798e090928410ac2156fafeeace28b439d66b466
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2021-09-09 11:54:42 +0200

    [14.0][MIG] currency_rate_inverted

--- currency_rate_inverted/models/res_currency.py
+++ currency_rate_inverted/models/res_currency.py
@@ -1,4 +1,4 @@
-# Copyright 2018 ForgeFlow, S.L.
+# Copyright 2021 ForgeFlow, S.L.
 # Copyright 2015 Techrifiv Solutions Pte Ltd
 # Copyright 2015 Statecraft Systems Pte Ltd
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
