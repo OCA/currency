79755f79fe3102baf461d58e1d8f85cf56312be9
Yurii Razumovskyi <GarazdCreation@gmail.com>
2022-10-11 13:19:31 +0300

    [ADD] currency_rate_update_nbu

---
+++ currency_rate_update_nbu/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Garazd Creation
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Currency Rate Update NBU",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "summary": "Allows to download currency exchange rates from "
+    "National Bank of Ukraine",
+    "author": "Garazd Creation," "Odoo Community Association (OCA),",
+    "website": "https://github.com/OCA/currency",
+    "license": "AGPL-3",
+    "depends": [
+        "currency_rate_update",
+    ],
+    "data": [
+        "wizards/res_currency_rate_update_wizard.xml",
+    ],
+    "installable": True,
+}
---
+++ currency_rate_update_nbu/models/res_currency_rate_provider_nbu.py
@@ -0,0 +1,160 @@
+# Copyright 2022 Garazd Creation
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+import json
+import logging
+from collections import defaultdict
+
+import requests
+from requests.exceptions import Timeout, TooManyRedirects
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+ALLOWED_BASE_CURRENCIES = [
+    "AUD",
+    "BRL",
+    "CAD",
+    "CHF",
+    "CLP",
+    "CNY",
+    "CZK",
+    "DKK",
+    "EUR",
+    "GBP",
+    "HKD",
+    "HUF",
+    "IDR",
+    "ILS",
+    "INR",
+    "JPY",
+    "KRW",
+    "MXN",
+    "MYR",
+    "NOK",
+    "NZD",
+    "PHP",
+    "PKR",
+    "PLN",
+    "RUB",
+    "SEK",
+    "SGD",
+    "THB",
+    "TRY",
+    "TWD",
+    "USD",
+    "ZAR",
+]
+
+
+class ResCurrencyRateProviderNBU(models.Model):
+    """Implementation for National Bank of Ukraine"""
+
+    _inherit = "res.currency.rate.provider"
+
+    service = fields.Selection(
+        selection_add=[("NBU", "National Bank of Ukraine")],
+        ondelete={"NBU": "set default"},
+    )
+
+    def _get_supported_currencies(self):
+        self.ensure_one()
+        if self.service != "NBU":
+            return super()._get_supported_currencies()
+        # List of currencies obrained from:
+        # https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json
+        return [
+            "USD",
+            "EUR",
+        ]
+
+    def _process_request(self, url, params, headers):
+        try:
+            response = requests.get(url=url, params=params, headers=headers, timeout=60)
+            response_data = json.loads(response.text)
+            status = response_data.get("status")
+            if status and status.get("error_code", False):
+                raise Exception(
+                    _(
+                        "Failed to fetch from CoinMarketCap with error code: "
+                        "%s and error message: %s"
+                    )
+                    % (status.get("error_code"), status.get("error_message"))
+                )
+        except (ConnectionError, Timeout, TooManyRedirects) as e:
+            raise Exception(str(e))
+        return response_data
+
+    def _get_latest_rate(self, currencies, base_currency, headers):
+        content = defaultdict(dict)
+        url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+        params = {}
+        #     "symbol": ",".join(currencies),
+        #     "convert": base_currency,
+        # }
+        data = self._process_request(url, params, headers)
+        for currency in data:
+            quote = vals.get("quote", {})
+            if quote:
+                convert_currency = quote.get(base_currency, {})
+                if convert_currency:
+                    timestamp = convert_currency.get("last_updated")[:10]
+                    rate = convert_currency.get("price", 0)
+                    content[timestamp].update({currency: rate})
+        return content
+
+    def _get_historical_rate(
+        self, currencies, date_from, date_to, base_currency, headers
+    ):
+        content = defaultdict(dict)
+        url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/historical"
+        params = {
+            "symbol": ",".join(currencies),
+            "time_start": date_from,
+            "time_end": date_to,
+            "interval": "1d",
+            "convert": base_currency,
+        }
+        data = self._process_request(url, params, headers)
+        for currency, vals in data.items():
+            for quote in vals.get("quotes", {}):
+                timestamp = quote.get("timestamp")[:10]
+                convert_currency = quote.get("quote", {}).get(base_currency, {})
+                if convert_currency:
+                    rate = convert_currency.get("price", 0)
+                    content[timestamp].update({currency: rate})
+        return content
+
+    @api.model
+    def _obtain_rates(self, base_currency, currencies, date_from, date_to):
+        self.ensure_one()
+        if "CMC" not in self.service:
+            return super()._obtain_rates(base_currency, currencies, date_from, date_to)
+        if base_currency not in ALLOWED_BASE_CURRENCIES:
+            raise UserError(
+                _("Company currency %s is not allowed in " "CoinMarketCap service ")
+                % base_currency
+            )
+        if base_currency in currencies:
+            currencies.remove(base_currency)
+
+        API_KEY = self.env["ir.config_parameter"].get_param("X-CMC_PRO_API_KEY")
+        if not API_KEY:
+            raise UserError(
+                _(
+                    "API KEY not found in System Parameters. Make sure to "
+                    "define the API KEY using the key X-CMC_PRO_API_KEY"
+                )
+            )
+        headers = {
+            "Accepts": "application/json",
+            "X-CMC_PRO_API_KEY": API_KEY,
+        }
+
+        if self.service == "CMC Standard":
+            return self._get_historical_rate(
+                currencies, date_from, date_to, base_currency, headers
+            )
+        else:
+            return self._get_latest_rate(currencies, base_currency, headers)
