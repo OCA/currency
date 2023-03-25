519a22887439f6076593f908dc41e3dce2af9934
yibudak <yibudak@gmail.com>
2023-03-25 21:28:13 +0300

    [16.0][FIX] pre-commit fixes 2

--- currency_rate_update_tcmb/models/res_currency_rate_provider_TCMB.py
+++ currency_rate_update_tcmb/models/res_currency_rate_provider_TCMB.py
@@ -1,9 +1,11 @@
 # Copyright 2023 Yiğit Budak (https://github.com/yibudak)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import logging
+from datetime import timedelta, date
+
 import requests
 from lxml.etree import fromstring
-from datetime import timedelta, date
+
 from odoo import models, fields, _
 
 _logger = logging.getLogger(__name__)
