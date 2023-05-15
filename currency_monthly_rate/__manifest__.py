be4580241ead44c4391b96c55f06d71b492239b2
kaynnan <kaynnanx1@gmail.com>
2023-05-15 18:42:36 -0300

    [MIG] currency_monthly_rate: Migration to 14.0

--- currency_monthly_rate/__manifest__.py
+++ currency_monthly_rate/__manifest__.py
@@ -2,16 +2,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Currency Monthly Rate",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Generic Modules/Accounting",
     "website": "https://github.com/OCA/currency",
-    "depends": ['base'],
+    "depends": ["base"],
     "data": [
-        'security/res_groups.xml',
-        'security/ir.model.access.csv',
-        'views/res_currency.xml',
+        "security/res_groups.xml",
+        "security/ir.model.access.csv",
+        "views/res_currency.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
