f75d5069480dbbc090f7e48f36790b27a5ef2e34
Alexis de Lattre <alexis.delattre@akretion.com>
2023-06-09 23:52:54 +0200

    currency_rate_update: fix bad strings
    Minor code cleanup

--- currency_rate_update/models/res_config_settings.py
+++ currency_rate_update/models/res_config_settings.py
@@ -9,8 +9,6 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     currency_rates_autoupdate = fields.Boolean(
-        string="Automatic Currency Rates (OCA)",
         related="company_id.currency_rates_autoupdate",
         readonly=False,
-        help="Enable regular automatic currency rates updates",
     )
