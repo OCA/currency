390e096a7889f84275a7f5031c46cd8dde3dcaad
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-08-27 13:17:15 +0200

    Add copier template

--- currency_rate_update_xe/__manifest__.py
+++ currency_rate_update_xe/__manifest__.py
@@ -6,6 +6,7 @@
     'name': 'Currency Rate Update: XE.com',
     'version': '12.0.1.0.0',
     'category': 'Financial Management/Configuration',
+    'website': 'https://github.com/OCA/currency',
     'summary': 'Update exchange rates using XE.com',
     'author':
         'CorporateHub, '
