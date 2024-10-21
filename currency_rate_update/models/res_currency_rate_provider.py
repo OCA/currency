04d14654b4b22a34ff91bd240370e7e4725acac3
Alex Cuellar Morales <acuellar@grupoyacck.com>
2024-10-21 13:14:53 -0500

    [IMP] currency_rate_update: pre-commit auto fixes

--- currency_rate_update/models/res_currency_rate_provider.py
+++ currency_rate_update/models/res_currency_rate_provider.py
@@ -140,12 +140,8 @@ class ResCurrencyRateProvider(models.Model):
             except BaseException as e:
                 _logger.warning(
                     (
-                        'Currency Rate Provider "{name}" failed to obtain data since'
-                        " {date_from} until {date_to}"
-                    ).format(
-                        name=provider.name,
-                        date_from=date_from,
-                        date_to=date_to,
+                        f'Currency Rate Provider "{provider.name}" failed to obtain data since'
+                        f" {date_from} until {date_to}"
                     ),
                     exc_info=True,
                 )
