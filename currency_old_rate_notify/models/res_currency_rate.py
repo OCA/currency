f6f3915f2964d12f690e940b1703192362ca2c9f
Alexis de Lattre <alexis.delattre@akretion.com>
2024-12-05 22:03:46 +0100

    [MIG] currency_old_rate_notify from v16 to v18

--- currency_old_rate_notify/models/res_currency_rate.py
+++ currency_old_rate_notify/models/res_currency_rate.py
@@ -41,10 +41,6 @@ class ResCurrencyRate(models.Model):
                 "title": _("Currency rates older than %d days", max_days),
                 "sticky": True,
                 "message": f"<ul>{''.join(msg)}</ul>",
-                # TODO remove once bug is fixed in the PR to mig web_notify in v16
-                # https://github.com/OCA/web/pull/2412
-                # We also need the fix for HTML in "message
-                "target": user.partner_id,
             }
         return res
 
