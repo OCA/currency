dc5989ab4cd46e2d319b2e8a284b3cfb7c1e235a
Alex Cuellar <acuellar@grupoyacck.com>
2025-10-12 14:24:12 -0500

    [IMP] currency_rate_update: pre-commit auto fixes

--- currency_rate_update/__manifest__.py
+++ currency_rate_update/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Currency Rate Update",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.1",
     "author": "Camptocamp, CorporateHub, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/currency",
     "license": "AGPL-3",
